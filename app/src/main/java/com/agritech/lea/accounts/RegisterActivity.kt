72072f0f59d368161d1c45849267825e633556b7
pirupius <piruville@gmail.com>
2018-06-13 00:03:44 +0300

    adding navigation

---
+++ app/src/main/java/com/agritech/lea/accounts/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.agritech.lea.accounts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.agritech.lea.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
